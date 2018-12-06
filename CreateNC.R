98e98dbf6e716769ca3ab920cc59139457cd2f96
Katie Harrington <katiejharrington@gmail.com>
2018-12-06 15:36:24 -0800

    Deleting unused scripts

--- CreateNC.R
+++
@@ -1,8 +0,0 @@
-##########################################
-####        Create NC file           #####
-##########################################
-
-# Import rediscretized data
-filename <- file.choose()
-data48hz <- read_csv(filename)
-depid <- basename(filename) # add code to remove "48HZ-" and "-COMBINED.csv"
