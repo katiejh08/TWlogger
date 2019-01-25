12586f0caeb2f339486a7f0e54965c35d53192f5
katiejharrington@gmail.com <katiejharrington@gmail.com>
2019-01-25 10:49:59 -0800

    Cleaned up the code

--- AnalyzeFinalAcc.R
+++ AnalyzeFinalAcc.R
@@ -1,16 +1,22 @@
-########################################
-####         Import Acc Data       #####
-########################################
 library(tidyverse)
 library(dplyr)
 library(zoo)
 library(tagtools)
 library(lubridate)
+library(plotly)
+library(ggplot2)
+library(RcppRoll)
+library(car)
+library(gridExtra)
 
 setwd("~/Projects/R/TWlogger")
 tzOffset <-"Etc/GMT+3"
-# Import calibrated rediscretized data
+
+########################################
+####         Import Acc Data       #####
+########################################
 filename <- file.choose()
+# Import calibrated rediscretized final acceleration data
 data <- read_csv(filename, 
                  col_types = cols(
                    #dttz = col_datetime(),
@@ -25,10 +31,16 @@ data <- read_csv(filename,
                    freq = col_double(), # Only comment this out for 2017 tags
                    secs_since = col_double()))
 
+# Import metrics file
+data <- read_csv(filename, 
+                 col_types = cols(
+                   dt = col_datetime()))
+
+
 ########################################
 ####       Confirm Time Zone       #####
 ########################################
-# Check dttz. Dttz has not been retaining time zone when writing to CSV. 
+# Check dttz. Dttz has not been retaining time zone when writing to CSV 
 attr(data$dttz, "tzone") #Check tz
 attr(data$dttz, "tzone") <- "Etc/GMT+3"
 
@@ -59,112 +71,94 @@ attr(data$dttz, "tzone") <- "Etc/GMT+3"
 attr(data$dttz, "tzone") #Check tz
 head(data$dttz)
 
-########################################
-####     Create Deployment ID      #####
-########################################
+############################################
+####  Create Deployment ID or Bird ID  #####
+############################################
+## Create deployment ID
 # For 2018 tags
 depid <- basename(filename)
 depid <- strsplit(depid,'-')
 depid <- depid[[1]][1]
 depid
 
-# For 2017 tags (if necessary)
+# For 2017 tags (must force manually)
 depid <- "20170214_Tag6_P25"
 
-# If need to override pre existing final files to now have correct tz in all final files
-write_csv(data, file.path(dirname(filename), paste(depid,"-Final.csv",sep="")))
-########################################
-####          Plot Acc Data        #####
-########################################
-library(tidyverse)
-library(plotly)
-library(ggplot2)
+## Create Bird ID
+depid2 <- strsplit(depid,'_')
+depid2[[1]][3]
+data2$ID <- depid2[[1]][3] # Add bird ID to metrics file; may need to change dataframe depending on subset
 
-# Subset data to only include Acc
+####################################
+####        Subset Data        #####
+####################################
+## Subset acc raw data to only include acc
 data2 <- data[,c("dttz","true_since","Ax","Ay","Az")]
 
-# Specify the start and end time of segment of interest (NOTE: change for each deployment)
-startTime <- as.POSIXct(strptime("2018-07-8 18:30:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
-endTime <- as.POSIXct(strptime("2018-07-09 18:30:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
+## Create 24-hr subsets 
+#Specify the start and end time of segment of interest to create
+startTime <- as.POSIXct(strptime("2018-07-9 06:00:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
+endTime <- as.POSIXct(strptime("2018-07-09 09:00:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
 # Run subset() function to extract data for the selected timerange
 data2 <- subset(data2, data2$dttz >= startTime & data2$dttz <= endTime)
 
-##### Create static plot of complete data
-# NOTE: use dttz when requiring time of day for x-axis AND use true_since when requiring extreme zoom
-p <- data4 %>%
-  gather(axis, acc, Ax:Az) %>%
-  ggplot(aes(true_since, acc, color = axis)) +
-  geom_line() +
-  theme_classic() +
-  labs(x = "Time", y = "Acceleration")
-p
-
-##### Plot successive subsets of data and save to file
-chunkLength <- 30 # Set this each time: length (in minutes) of chunks to plot
-# i = 1 # Can set i to number of plots wanted and just run what's within for loop from test to p
-# test$dttz[i] # could also name using the hh:mm of segment instead of i
-
-for(i in 1:ceiling(nrow(data2)/(chunkLength *60*50))) {
-  test <- data2[seq(((i-1)*chunkLength*50*60)+1,i*chunkLength*50*60),]
-  # ggplot() +
-  #   geom_line(data = test, aes(x = dttz, y = Ax,color = 'AX')) +
-  #   geom_line(data = test, aes(x = dttz, y = Ay,color = 'AY')) +
-  #   geom_line(data = test, aes(x = dttz, y = Az,color = 'AZ')) +
-  #   scale_colour_manual(name="Axis",
-  #                       values=c(AX="red", AY="blue", AZ="green")) +
-  #   ylab("Raw ACC") + 
-  #   xlab("Time")
-  p <- test %>%
-    gather(axis, acc, Ax:Az) %>%
-    ggplot(aes(dttz, acc, color = axis)) +
-    geom_line() +
-    theme_classic() +
-    labs(x = "Time", y = "Acceleration") +
-    ggtitle(depid)
-  p
-  # save plots as .png
-  ggsave(p, file=paste(depid,'-',i, ".png", sep=''), scale=2)
-}
-
-##### Create interactive plot
-# ggplotly(p)
+## Create more narrow subsets (First calculate metrics below)
+startMetrics <- as.POSIXct(strptime("2018-07-9 06:00:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
+endMetrics <- as.POSIXct(strptime("2018-07-09 09:00:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
+# Extract data for the selected timerange
+# Note: data3 is expanded metrics and data4 is abbreviated metrics; must choose which one to subset
+data5 <- subset(data3, data2$dttz >= startMetrics & data2$Metrics <= endTime)
 
 ##########################################
 ####       Calculate Metrics         #####
 ##########################################
-library(RcppRoll)
+## Magnitude of acceleration
+Ax <-data2$Ax
+Ay <-data2$Ay
+Az <-data2$Az
+Ax2 <-Ax^2
+Ay2 <-Ay^2
+Az2 <-Az^2
+Amag <- Ax2 + Ay2 + Az2
+Amag <-sqrt(Amag)
 
-# Calculate ODBA (Need to read about Wilson method, filter pass, and n)
+## Running mean of magnitude of acceleration
+Amag_rollmean <- roll_mean(Amag,50,fill=NA)
+Amag_rollmean[seq(1,24)] <- Amag_rollmean[25]
+Amag_rollmean[seq(length(Amag_rollmean)-24,length(Amag_rollmean))] <- Amag_rollmean[length(Amag_rollmean)-25]
+
+## ODBA (Need to read about Wilson method, filter pass, and n)
 A <- cbind(data2$Ax, data2$Ay, data2$Az)
 odba <- odba(A, sampling_rate = 50,method="wilson",n = 50) # n is sampling window, e.g. 50=1s
+odba2 <- odba(A, sampling_rate = 50,method="wilson",n = 100)
+data5 <-cbind.data.frame(dt,true_since,odba2)
 # Plot ODBA
 # ba <- list(odba = odba)
 # plott(ba, 50) # NOTE: Change if different sampling rate
 
-# Calculate pitch and roll (NOTE: will be in radians)
+## Pitch and roll (NOTE: calculates in radians)
 pr <- a2pr(A,50)
 #prlist <-list(pitch=pr$p,roll=pr$r)
 #plott(prlist,fs=50)
 p <- pr$p
 r <-pr$r
 
-# Calculate jerk
+## Jerk
 jerk <-njerk(A,sampling_rate=50)
 # jerklist <-(jerk=jerk)
 # plott(jerklist,fs=50)
 
-# Calculate msa
+## MSA (minimum specific acceleration)
 msa <-msa(A)
 #msalist <- list(msa = msa)
 #plott(msalist,fs=50)
 
-# Calculate norm
+##  Norm
 normAcc <- norm2(A)
 # normlist <- list(normAcc = normAcc)
 # plott(normlist,fs=50)
 
-# Calculate rolling means and variances
-# Replace top NA values with the 1st calculated value and last NAs with last caluculated value. 
+## Running means and variances for each axis
 mheave <-roll_mean(data2$Az, n=50, fill=NA) # fill=NA replaced missing values created by moving window (e.g.first 24 and last 25 rows were empty) 
 mheave[seq(1,24)] <- mheave[25]
 mheave[seq(length(mheave)-24,length(mheave))] <- mheave[length(mheave)-25]
@@ -189,105 +183,132 @@ varsway <- roll_var(data2$Ay, n=50, fill=NA)
 varsway[seq(1,24)] <- varsway[25]
 varsway[seq(length(varsway)-24,length(varsway))] <- varsway[length(varsway)-25]
 
-##### Plot means and variances. First Select axis to plot.
+## Plot means and variances. First Select axis to plot.
 # z <- varheave
 # z <- varsurge
 # z <- varsway
 # varlist <- list(z = z)
 # plott(varlist,fs=50)
 
-##### Plot multiple metrics
+## Plot multiple metrics
 # overallList <- list(jerk = jerk, odba = odba,msa = msa)
 # plott(overallList,fs=50)
 
-#### Combine metrics into new df
-dt <-data2$dttz
+##########################################
+####   Combine metrics into new df   #####
+##########################################
+dttz <-data2$dttz
 true_since <-data2$true_since
 # Create expanded metrics file
-data3 <-cbind.data.frame(dt,true_since,odba,jerk,mheave,varheave,msurge,varsurge,msway,varsway,msa, p,r,normAcc)
+data3 <-cbind.data.frame(dttz,true_since,Amag_rollmean,odba,jerk,mheave,varheave,msurge,varsurge,msway,varsway,msa, p,r,normAcc)
 # Create abbreviated metrics file
-data4 <-cbind.data.frame(dt,true_since,odba,jerk,p,r)
+data4 <-cbind.data.frame(dttz,true_since,Amag_rollmean,odba,jerk,p,r)
 # Check for NAs
 sapply(data4, function(x) sum(is.na(x)))
 str(data4)
-# Add bird ID
-depid2 <- strsplit(depid,'_')
-depid2[[1]][3]
-data4$ID <- depid2[[1]][3]
-
-write_csv(data4, file.path(dirname(filename), paste(depid,"-metrics.csv",sep="")))
 
 ##########################################
-####       Import Metrics File       #####
+####       Downsample Metrics        #####
 ##########################################
-# NOTE: p (pitch) and r (roll) are in radians
-
-filename2 <- file.choose()
-test <- read_csv(filename2, 
-                 col_types = cols(
-                   dt = col_datetime()))
-                   # true_since = col_datetime(),
-                   # odba = col_double(),
-                   # jerk = col_double(),
-                   # mheave = col_double(),
-                   # varheave = col_double(),
-                   # msurge = col_double(),
-                   # varsurge = col_double(),
-                   # msway = col_double(),
-                   # varsway = col_double(),
-                   # msa = col_double(),
-                   # p = col_double(),
-                   # r = col_double(),
-                   # normAcc = col_double()))
-
-# Subset metrics
-tzOffset <-"Etc/GMT+3"
-startMetrics <- as.POSIXct(strptime("2018-07-08 8:55:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
-endMetrics <- as.POSIXct(strptime("2018-07-09 16:58:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
-# Run subset() function to extract data for the selected timerange
-data3 <- subset(data, data$dttz >= startMetrics & data$dttz <= endMetrics)
+# onehz <- decdc()
 
 ##########################################
-####      Explore the Metrics        #####
+####          Create Plots           #####
 ##########################################
-library(car)
-library(gridExtra)
-
-# Choose which metric to explore
-x <- data4$odba 
+# Choose which metric to plot
+x <- data4$odba # may need to change dataframe from which it is drawing
 #x <- log(data4$odba)
 
-# Plot histograms, density plots, ecdf
+## Exploratory plots: histograms, density, ecdf, q-q
 hist(x, main="Histogram of observed data")
 hist(x,freq=F)
 lines(density(x))
 plot(density(x), main="Density Estimate of Data") #KD Plot returns the density data
 plot(ecdf(x),main="Empirical cumulative distribution function")
+z.norm<-(x-mean(x))/sd(x) # Standardized data
+qqnorm(z.norm); qqline(z.norm) # Draw a QQplot with a 45-degree reference line
 
-# Standardized data
-z.norm<-(x-mean(x))/sd(x)
-# Draw a QQplot with a 45-degree reference line
-qqnorm(z.norm); qqline(z.norm) 
+## Create static plots
+# NOTE: use dttz when requiring time of day for x-axis AND use true_since when requiring extreme zoom
 
-# Stack multiple plots to view ODBA, jerk, pitch and roll together
+# Run the following two lines to create 3 stacked plots including P1, P2, and P3
+# par(mfrow=c(3,1))
+# require(gridExtra)
+
+# Plot acc
+p1 <- data2 %>%
+  gather(axis, acc, Ax:Az) %>%
+  ggplot(aes(dttz, acc, color = axis)) +
+  theme(legend.position="top") +
+  geom_line() +
+  theme_classic() +
+  labs(x = "Time", y = "Acceleration")
+p1
+
+# Plot Amag (or swap out other metric)
+p2 <- data2 %>%
+  ggplot(aes(dttz, Amag),color = 'black') +
+  geom_line() +
+  theme_classic() +
+  labs(x = "Time", y = "Acc mag")
+p2
+
+# Plot Amag_rollmean (or swap out other metric)
+p3 <- data2 %>%
+  ggplot(aes(x = dttz, y = Amag_rollmean),color= 'black') +
+  geom_line() +
+  labs(x = "Time", y = "Running mean acc mag")
+p3
+
+# Run this to create stacked plots 
+# grid.arrange(p1, p2, p3, nrow=3)
+
+## Create ODBA plot
+op <- data4 %>% # May need to change dataframe from which it's drawing
+  ggplot(aes(dt, odba),color = 'black') +
+  geom_line() +
+  theme_classic() +
+  labs(x = "Time", y = "ODBA")
+op
+
+## Plot successive subsets of data and save to file
+chunkLength <- 30 # Set this each time: length (in minutes) of chunks to plot
+# i = 1 # Can set i to number of plots wanted and just run what's within for loop from test to p
+# test$dttz[i] # could also name using the hh:mm of segment instead of i though need to build this out
+
+for(i in 1:ceiling(nrow(data2)/(chunkLength *60*50))) {
+  test <- data2[seq(((i-1)*chunkLength*50*60)+1,i*chunkLength*50*60),]
+  p <- test %>%
+    gather(axis, acc, Ax:Az) %>%
+    ggplot(aes(dttz, acc, color = axis)) +
+    geom_line() +
+    theme_classic() +
+    labs(x = "Time", y = "Acceleration") +
+    ggtitle(depid)
+  p
+  # save plots as .png
+  ggsave(p, file=paste(depid,'-',i, ".png", sep=''), scale=2)
+}
+
+## Create stacked plots of successive subsets of data
 chunkLength <- 10 # length in minutes of chunk
 i=1
 
 for(i in 1:ceiling(nrow(data3)/(chunkLength *60*50))) {
   test <- data3[seq(((i-1)*chunkLength*50*60)+1,i*chunkLength*50*60),]
   par(mfrow=c(3,1))
-  p <- test %>%
+  p1 <- test %>%
     gather(axis, acc, ax:az) %>%
     ggplot(aes(true_since, acc, color = axis)) +
     geom_line() +
     theme_classic() +
     labs(x = "Time", y = "Acceleration")
-  op <- test %>%
+  p2 <- test %>%
     ggplot(aes(dttz, odba),color = 'black') +
     geom_line() +
     theme_classic() +
     labs(x = "Time", y = "ODBA")
-  prp <- test %>%
+  p3 <- test %>%
     ggplot() +
     geom_line(aes(x=dttz, y=r*(180/pi), color='blue')) +
     geom_line(aes(x=dttz, y=p*(180/pi), color='red')) +
@@ -296,6 +317,40 @@ for(i in 1:ceiling(nrow(data3)/(chunkLength *60*50))) {
   # save plots as .png
   ggsave(p, file=paste(depid,'-',i, ".png", sep=''), scale=2)
 }
+
+## Create interactive plot (unfinished)
+# ggplotly(p)
+
+##########################################
+####              HMM                #####
+##########################################
+library(momentuHMM)
+# HMM is better than kmeans because of the serial dependence of acc data
+# Code coming from Roland Langrock
+
+##########################################
+####         Write to CSV            #####
+##########################################
+## Convert dttz back to dt (for some reason dttz does not retain tz when re-imported; always imports as UTC)
+dt <-data2$dttz
+# As needed, modify columns to be included
+final <-cbind.data.frame(dt,true_since,Amag_rollmean,odba,jerk,mheave,varheave,msurge,varsurge,msway,varsway,msa, p,r,normAcc)
+# Create abbreviated metrics file
+final2 <-cbind.data.frame(dt,true_since,Amag_rollmean,odba,jerk,p,r)
+
+# Change dataframe and file name addition as needed
+write_csv(final, file.path(dirname(filename), paste(depid,"-metrics.csv",sep="")))
+write_csv(final2, file.path(dirname(filename), paste(depid,"-acc.csv",sep="")))
+
+
+
+
+##########################################
+#####
+#####        Boneyard Scripts         
+#####
+##########################################
+# All following scripts may be used to at future point, but not currently.
 
 ##########################################
 ####        Fit Distribution         #####
@@ -340,54 +395,3 @@ for(i in 1:ceiling(nrow(data3)/(chunkLength *60*50))) {
 #                        , scale = fit.weibull$estimate["scale"])$statistic
 #                , fit
 # )
-
-##########################################
-####              HMM                #####
-##########################################
-library(momentuHMM)
-# HMM is better than kmeans because of the serial dependence of acc data
-# Code coming from Roland Langrock
-
-##########################################
-####              PCA                #####
-##########################################
-# Run PCA to determine which metrics explain the most variance in the data
-# Not doing this, since ODBA and jerk are accepted (would only do because it's interesting)
-
-# library(devtools)
-# library("ggbiplot")
-# 
-# data4.pca <- prcomp(data4[,2:10], center = TRUE,scale. = TRUE)
-# summary(data4.pca)
-# ggbiplot(data4.pca,ellipse=TRUE,obs.scale = 1,var.scale = 1) +
-#   ggtitle("PCA of tag metrics")+
-#   theme_minimal()
-
-##########################################
-####           k means               #####
-##########################################
-# Haven't fully adapted the following, though I don't plan to use.
-
-# library(plotly)
-# library(tidyverse)
-# # Classify data
-# # kmeans assumes independence between individual observations, which is not true for acc data)
-# set.seed(20)
-# clusters <- kmeans(data4[,2:4], 2)
-# data4$states <- as.factor(clusters$cluster)
-# str(clusters)
-# 
-# # Plot clusters (NOTE: This is not the best way to plot)
-# k <- plot_ly(data4, x = ~o, y = ~jerk, z = ~varheave, color = ~states, colors = c('#BF382A', '#0C4B8E')) %>%
-#   add_markers() %>%
-#   layout(scene = list(xaxis = list(title = 'ODBA'),
-#                       yaxis = list(title = 'Jerk'),
-#                       zaxis = list(title = 'Variance of Heave')))
-# k
-
-op <- data4 %>%
-  ggplot(aes(dt, odba),color = 'black') +
-  geom_line() +
-  theme_classic() +
-  labs(x = "Time", y = "ODBA")
-op
