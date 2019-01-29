69a38ef408ac9a29d8de23e1ca5ec8a15513e632
katiejharrington@gmail.com <katiejharrington@gmail.com>
2019-01-28 17:10:02 -0800

    Updated with decdc

--- AnalyzeFinalAcc.R
+++ AnalyzeFinalAcc.R
@@ -45,14 +45,20 @@ data <- read_csv(filename,
 #                    dt = col_datetime()))
 
 # Import raw acc and metrics file
-# data <- file.choose()
-# test <- read_csv(filename, 
+# filename <- file.choose()
+# data <- read_csv(filename, 
 #                  col_types = cols(
 #                    dt = col_datetime(),
 #                    Ax = col_double(),
 #                    Ay = col_double(),
 #                    Az = col_double(),
 #                    Amag_rollmean = col_double()))
+
+# Import down sampled data
+filename <- file.choose()
+data <- read_csv(filename,
+                 col_types = cols(
+                   dttz_down = col_datetime()))
 
 ########################################
 ####       Confirm Time Zone       #####
@@ -115,7 +121,7 @@ data2 <- data[,c("dttz","true_since","Ax","Ay","Az")]
 startTime <- as.POSIXct(strptime("2018-07-8 18:30:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
 endTime <- as.POSIXct(strptime("2018-07-09 18:30:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
 # Run subset() function to extract data for the selected timerange
-data2 <- subset(data2, data2$dttz >= startTime & data2$dttz <= endTime)
+down_24hr <- subset(down, down$dttz_down >= startTime & down$dttz_down <= endTime)
 
 ## Create more narrow subsets (First calculate metrics below)
 startMetrics <- as.POSIXct(strptime("2018-07-9 06:00:00",format="%Y-%m-%d %H:%M:%S"),tz=tzOffset)
@@ -128,15 +134,51 @@ data5 <- subset(data3, data2$dttz >= startMetrics & data2$Metrics <= endTime)
 ##########################################
 ####         Downsample Data         #####
 ##########################################
+# decdc runs a low pass filter (essentially a running mean)
+# meaning it knocks out noise and interpolates
+# every 10th of a second we've come up with a decimated (averaged) value 
+# which represents the window of values at that time
+
 # Decimation factor of 5 downsamples to 10Hz
-down <- data.matrix(data2)
-down <- decdc(down,5)
+# Decimate each vector separately
+df <- 10 # Set decimation factor df
+fs <- 10 # Set original sampling rate
+# For datetime select ever nth value
+dttz <- data2$dttz
+a <- dttz
+dttz_down <- a[seq(1, length(a), df)]
+
+true_since <- data2$true_since
+a <- true_since
+true_since_down <- a[seq(1, length(a), df)]
+
+
+Ax <- data2$Ax
+Ay <- data2$Ay
+Az <- data2$Az
+# Convert vectors to numeric matrix
+Ax_mat <- matrix(Ax,ncol=1)
+Ay_mat <- matrix(Ay,ncol=1)
+Az_mat <- matrix(Az,ncol=1)
+Ax_down <- decdc(Ax_mat,df)
+Ay_down <- decdc(Ay_mat,df)
+Az_down <- decdc(Az_mat,df)
+
+data2_down <- cbind.data.frame(dttz_down,true_since_down,Ax_down,Ay_down,Az_down)
+down <-data2_down
+# Very important to run this line to reset sampling rate to down sampled rate
+fs = fs/df
+
+# Historic attempt:
+# Select every nth value from datetime
+# down <- data.matrix(data2)
+# down <- decdc(down,5)
 
 ##########################################
 ####       Calculate Metrics         #####
 ##########################################
-fs <- 50 # Specify sampling rate, which will be different based on decimation
-sw <- 50 # Specify sampling window (50 or 1s if at 50Hz, 10 or 1s if at 10Hz)
+fs # Confirm sampling rate matches decimated data
+sw <- fs # Specify sampling window (if equivalent to fs, then 1s window)
 
 ## Magnitude of acceleration
 Ax <-down$Ax
@@ -154,7 +196,7 @@ Amag_rollmean[seq(1,24)] <- Amag_rollmean[25]
 Amag_rollmean[seq(length(Amag_rollmean)-24,length(Amag_rollmean))] <- Amag_rollmean[length(Amag_rollmean)-25]
 
 ## ODBA (Need to read about Wilson method, filter pass, and n)
-A <- cbind(down$Ax, down$Ay, down$Az)
+A <- cbind(down$Ax_down,down$Ay_down,down$Az_down)
 odba <- odba(A, sampling_rate = fs,method="wilson",n = sw) # n is sampling window, e.g. 50=1s
 # Plot ODBA
 # ba <- list(odba = odba)
@@ -221,12 +263,16 @@ varsway[seq(length(varsway)-24,length(varsway))] <- varsway[length(varsway)-25]
 ##########################################
 ####   Combine metrics into new df   #####
 ##########################################
-dttz <-down$dttz
-true_since <-down$true_since
-# Create expanded metrics file
-data3 <-cbind.data.frame(dttz,true_since,Amag_rollmean,odba,jerk,mheave,varheave,msurge,varsurge,msway,varsway,msa, p,r,normAcc)
-# Create abbreviated metrics file
-data4 <-cbind.data.frame(dttz,true_since,Amag_rollmean,odba,jerk,p,r)
+# Combine down sampled acc data and metrics calculated using down sampled data
+down <- cbind.data.frame(dttz_down,true_since_down,Ax_down,Ay_down,Az_down,Amag_rollmean,odba)
+
+
+# dttz <-down$dttz
+# true_since <-down$true_since
+# # Create expanded metrics file
+# data3 <-cbind.data.frame(dttz,true_since,Amag_rollmean,odba,jerk,mheave,varheave,msurge,varsurge,msway,varsway,msa, p,r,normAcc)
+# # Create abbreviated metrics file
+# data4 <-cbind.data.frame(dttz,true_since,Amag_rollmean,odba,jerk,p,r)
 # Check for NAs
 sapply(data4, function(x) sum(is.na(x)))
 str(data4)
@@ -235,11 +281,11 @@ str(data4)
 ####          Create Plots           #####
 ##########################################
 # Choose which metric to plot
-x <- data4$odba # may need to change dataframe from which it is drawing
+x <- down_24hr$Amag_rollmean # may need to change dataframe from which it is drawing
 #x <- log(data4$odba)
 
 ## Exploratory plots: histograms, density, ecdf, q-q
-hist(x, main="Histogram of observed data")
+hist(x, main="Histogram of Amag_rollmean")
 hist(x,freq=F)
 lines(density(x))
 plot(density(x), main="Density Estimate of Data") #KD Plot returns the density data
@@ -265,7 +311,7 @@ p1 <- down %>%
 p1
 
 # Plot Amag (or swap out other metric)
-p2 <- down %>%
+p2 <- down_24hr %>%
   ggplot(aes(dttz, Amag),color = 'black') +
   geom_line() +
   theme_classic() +
@@ -273,8 +319,8 @@ p2 <- down %>%
 p2
 
 # Plot Amag_rollmean (or swap out other metric)
-p3 <- down %>%
-  ggplot(aes(x = dttz, y = Amag_rollmean),color= 'black') +
+p3 <- down_24hr %>%
+  ggplot(aes(x = dttz_down, y = Amag_rollmean),color= 'black') +
   geom_line() +
   labs(x = "Time", y = "Running mean acc mag")
 p3
@@ -283,8 +329,8 @@ p3
 # grid.arrange(p1, p2, p3, nrow=3)
 
 ## Create ODBA plot
-op <- down %>% # May need to change dataframe from which it's drawing
-  ggplot(aes(dt, odba),color = 'black') +
+op <- down_24hr %>% # May need to change dataframe from which it's drawing
+  ggplot(aes(dttz_down, odba),color = 'black') +
   geom_line() +
   theme_classic() +
   labs(x = "Time", y = "ODBA")
@@ -358,7 +404,7 @@ final <-cbind.data.frame(dt,true_since,Amag_rollmean,odba,jerk,mheave,varheave,m
 final2 <-cbind.data.frame(dt,true_since,Amag_rollmean,odba,jerk,p,r)
 
 # Change dataframe and file name addition as needed
-write_csv(final, file.path(dirname(filename), paste(depid,"-metrics.csv",sep="")))
+write_csv(down_24hr, file.path(dirname(filename), paste(depid,"-",fs,"Hz.csv",sep="")))
 write_csv(final2, file.path(dirname(filename), paste(depid,"-acc.csv",sep="")))
 
 
